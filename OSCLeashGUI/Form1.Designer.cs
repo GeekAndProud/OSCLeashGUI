08ddf8de44a5a2aea55e4607f6d392e676e67197
GeekAndProud <toms_@UNICORN>
2023-05-13 16:36:33 +0100

    Add project files.

---
+++ OSCLeashGUI/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace OSCLeashGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runDeadzone = new System.Windows.Forms.TextBox();
+            this.strengthMult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.horizontal = new System.Windows.Forms.TextBox();
+            this.vertical = new System.Windows.Forms.TextBox();
+            this.leashgrabbedLbl = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(675, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(141, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // runDeadzone
+            // 
+            this.runDeadzone.Location = new System.Drawing.Point(147, 80);
+            this.runDeadzone.Name = "runDeadzone";
+            this.runDeadzone.Size = new System.Drawing.Size(100, 26);
+            this.runDeadzone.TabIndex = 1;
+            this.runDeadzone.Text = "0.7";
+            // 
+            // strengthMult
+            // 
+            this.strengthMult.Location = new System.Drawing.Point(147, 112);
+            this.strengthMult.Name = "strengthMult";
+            this.strengthMult.Size = new System.Drawing.Size(100, 26);
+            this.strengthMult.TabIndex = 2;
+            this.strengthMult.Text = "1.2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(417, 83);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vertical:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Run Deadzone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(4, 115);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Strength multiplier";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Horizontal:";
+            // 
+            // horizontal
+            // 
+            this.horizontal.Location = new System.Drawing.Point(489, 112);
+            this.horizontal.Name = "horizontal";
+            this.horizontal.ReadOnly = true;
+            this.horizontal.Size = new System.Drawing.Size(100, 26);
+            this.horizontal.TabIndex = 7;
+            // 
+            // vertical
+            // 
+            this.vertical.Location = new System.Drawing.Point(489, 80);
+            this.vertical.Name = "vertical";
+            this.vertical.ReadOnly = true;
+            this.vertical.Size = new System.Drawing.Size(100, 26);
+            this.vertical.TabIndex = 8;
+            // 
+            // leashgrabbedLbl
+            // 
+            this.leashgrabbedLbl.AutoSize = true;
+            this.leashgrabbedLbl.Location = new System.Drawing.Point(12, 46);
+            this.leashgrabbedLbl.Name = "leashgrabbedLbl";
+            this.leashgrabbedLbl.Size = new System.Drawing.Size(167, 20);
+            this.leashgrabbedLbl.TabIndex = 9;
+            this.leashgrabbedLbl.Text = "Leash is NOT grabbed";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(417, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Outputs";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 153);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.leashgrabbedLbl);
+            this.Controls.Add(this.vertical);
+            this.Controls.Add(this.horizontal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.strengthMult);
+            this.Controls.Add(this.runDeadzone);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "OSCLeashGUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TextBox runDeadzone;
+        private System.Windows.Forms.TextBox strengthMult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox horizontal;
+        private System.Windows.Forms.TextBox vertical;
+        private System.Windows.Forms.Label leashgrabbedLbl;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ OSCLeashGUI/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime;
+using BuildSoft.OscCore;
+using BlobHandles;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using Rug.Osc;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace OSCLeashGUI
+{
+    public partial class Form1 : Form
+    {
+        ThreadStart leashSender;
+        Thread leashSend;
+        ThreadStart leashChecker;
+        Thread leashCheck;
+        OscClient Client = new OscClient("127.0.0.1", 9000);
+        static OscReceiver receiver;
+        static OscSender OSCsender;
+        static Thread thread;
+        static Thread threader;
+        //OscServer server = new OscServer(9001);
+        bool MessageValue;
+        public float XPos;
+        public float XNeg;
+        public float ZPos;
+        public float ZNeg;
+        float leashStretch;
+        static float VerticalOutput;
+        static float HorizontalOutput;
+        IPAddress localhost;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class leasher
+        {
+            public static bool isGrabbed { get; set; }
+            public static float Stretch { get; set; }
+            public static float WalkDeadzone {get; set;}
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int port = 9001;
+            localhost = IPAddress.Parse("127.0.0.1");
+            // Create the receiver
+            receiver = new OscReceiver(port);
+            OSCsender = new OscSender(localhost, 9000);
+            // Create a thread to do the listening
+            thread = new Thread(new ThreadStart(ListenLoop));
+            threader = new Thread(new ThreadStart(sendDeets));
+            // Connect the receiver
+            receiver.Connect();
+
+            // Start the listen thread
+            thread.Start();
+
+
+        }
+
+        private void ListenLoop()
+        {
+            try
+            {
+                while (receiver.State != OscSocketState.Closed)
+                {
+                    // if we are in a state to recieve
+                    if (receiver.State == OscSocketState.Connected)
+                    {
+                        // get the next message 
+                        // this will block until one arrives or the socket is closed
+                        OscPacket packet = receiver.Receive();
+                        if (packet.ToString().Contains("Leash_IsGrabbed"))
+                        {
+                            Console.WriteLine(packet.ToString().Substring(packet.ToString().LastIndexOf(",") + 1));
+                            if (bool.Parse(packet.ToString().Substring(packet.ToString().LastIndexOf(",") + 1)))
+                            {
+                                leashgrabbedLbl.Invoke(new Action(() => leashgrabbedLbl.Text = "Leash IS grabbed"));
+                                if (packet.ToString().Contains("Leash_Stretch"))
+                                {
+                                    float stretchy = float.Parse(packet.ToString());
+                                    if (stretchy > float.Parse(runDeadzone.Text))
+                                    {
+                                        Debug.WriteLine(packet.ToString());
+                                        if (packet.ToString().Contains("Leash"))
+                                        {
+                                            int index = packet.ToString().IndexOf(",") + 2;
+                                            string floaty = packet.ToString();
+                                            string onlyFloat = "";
+                                            int length = floaty.Length - 1;
+                                            if (index >= 0)
+                                            onlyFloat = floaty.Substring(index, length - index);
+                                            if (packet.ToString().Contains("Leash_X+"))
+                                            {
+                                                XPos = float.Parse(onlyFloat);
+                                            }
+                                            else if (packet.ToString().Contains("Leash_X-"))
+                                            {
+                                                XNeg = float.Parse(onlyFloat);
+                                            }
+                                            else if (packet.ToString().Contains("Leash_Z+"))
+                                            {
+                                                ZPos = float.Parse(onlyFloat);
+                                            }
+                                            else if (packet.ToString().Contains("Leash_Z-"))
+                                            {
+                                                ZNeg = float.Parse(onlyFloat);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                leashgrabbedLbl.Invoke(new Action(() => leashgrabbedLbl.Text = "Leash is NOT grabbed"));
+                            }
+                        }
+                    }
+
+                    VerticalOutput = Clamp((ZPos - ZNeg) * leashStretch * float.Parse(strengthMult.Text));
+                    HorizontalOutput = Clamp((XPos - XNeg) * leashStretch * float.Parse(strengthMult.Text));
+                    vertical.Invoke(new Action(() => vertical.Text = VerticalOutput.ToString()));
+                    horizontal.Invoke(new Action(() => horizontal.Text = HorizontalOutput.ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                // if the socket was connected when this happens
+                // then tell the user
+                if (receiver.State == OscSocketState.Connected)
+                {
+                    Debug.WriteLine("Exception in listen loop");
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+            Thread.Sleep(200);
+        }
+
+        private float Clamp(float v)
+        {
+            return (v < -1) ? -1 : (v > 1) ? 1 : v;
+        }
+
+        private void sendDeets()
+        {
+            if (leasher.isGrabbed == true)
+            {
+                if (leasher.Stretch > float.Parse(runDeadzone.Text))
+                {
+                    Client.Send("/input/Vertical", VerticalOutput);
+                    Client.Send("/input/Horizontal", HorizontalOutput);
+                }
+                vertical.Invoke(new Action(() => vertical.Text = VerticalOutput.ToString()));
+                horizontal.Invoke(new Action(() => vertical.Text = HorizontalOutput.ToString()));
+            }
+
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            thread.Abort();
+            threader.Abort();
+            receiver.Close();
+            Close();
+        }
+    }
+}
